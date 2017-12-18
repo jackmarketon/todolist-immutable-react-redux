aaa5525e9b9fd22af2cdff2ff2445c49baa8c865
Jackson Marketon <jmarketon@gmail.com>
2017-12-18 16:40:52 -0600

    Read default properly

--- app/config/defaultState.js
+++ app/config/defaultState.js
@@ -2,4 +2,21 @@ export default {
   data: {
     view: 'all',
   },
+  todos: [
+    {
+      id: '0',
+      text: 'Feed the Lizards',
+      isCompleted: false,
+    },
+    {
+      id: '1',
+      text: 'Feed the dog',
+      isCompleted: true,
+    },
+    {
+      id: '2',
+      text: 'Clean the kitchen',
+      isCompleted: false,
+    },
+  ],
 };
--- app/main.js
+++ app/main.js
@@ -10,6 +10,8 @@ import reducers from 'reducers';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import defaultState from 'config/defaultState';
 import App from 'components/app';
+import Todo from 'models/todo';
+import TodoList from 'models/todoList';
 import {
   browserSupportsAllFeatures,
   // loadScript,
@@ -21,9 +23,27 @@ if (browserSupportsAllFeatures()) {
   // loadScript('//.herokuapp.com/static/polyfills.bundle.js', main);
 }
 
+function reviver(key, value) {
+  if ('todos' === key) {
+    return new TodoList(value).map((item) => new Todo(item));
+  }
+  return Immutable.Iterable.isIndexed(value) ?
+    value.toList() :
+    value.toMap();
+}
+
 function main() {
   // Create a history (using browser history)
-  const initialState = Immutable.fromJS(defaultState);
+  let initialState = false;
+  if (localStorage) {
+    const stateInStorage = localStorage.getItem('todos');
+    if (stateInStorage) {
+      initialState = Immutable.fromJS(stateInStorage, reviver);
+    }
+  }
+  if (!initialState) {
+    initialState = Immutable.fromJS(defaultState, reviver);
+  }
 
   const store = createStore(
     reducers,
