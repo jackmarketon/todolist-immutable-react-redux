bd203e700661e410ee7b2861c264155a8229146a
Jackson Marketon <jmarketon@gmail.com>
2017-12-18 17:29:58 -0600

    path maybe?

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+var path = require('path');
 
 app
   .use('/static', express.static('static'))
   .get('/', function (request, response) {
-    response.sendFile('static/index.html');
+    response.sendFile(path.resolve(__dirname, './static/index.html'));
   })
   .listen(port);
